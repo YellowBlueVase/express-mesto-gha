81a503ec35e07122849ecca3c4bda8faf4f9c6f4
Kirill Shilin <kvshilin@icloud.com>
2023-01-11 14:12:06 +0300

    1st try 14 sprint

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const auth = require('./middlewares/auth');
-
-// подключаемся к серверу mongo
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -24,7 +23,8 @@ app.use(cookieParser());
 app.use('/', routerUsers);
 app.use(auth);
 app.use('/', routerCards);
-app.use((err, req, res) => {
+app.use(errors());
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
@@ -34,6 +34,8 @@ app.use((err, req, res) => {
         ? 'На сервере произошла ошибка'
         : message,
     });
+
+  next();
 });
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,32 @@
+/* eslint-disable no-useless-escape */
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
   getAllUsers, getUser, getProfile, updateProfile, updateAvatar, login, createUser,
 } = require('../controllers/users');
 
-router.get('/users', auth, getAllUsers);
+const celebrateUser = {
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }).unknown(true),
+};
 
-router.get('/users/:userId', auth, getUser);
+router.get('/users', celebrate(celebrateUser), auth, getAllUsers);
 
-router.get('/users/me', auth, getProfile);
+router.get('/users/:userId', celebrate(celebrateUser), auth, getUser);
 
-router.patch('/users/me', auth, updateProfile);
+router.get('/users/me', celebrate(celebrateUser), auth, getProfile);
 
-router.patch('/users/me/avatar', auth, updateAvatar);
+router.patch('/users/me', celebrate(celebrateUser), auth, updateProfile);
 
-router.post('/signin', login);
+router.patch('/users/me/avatar', celebrate(celebrateUser), auth, updateAvatar);
+
+router.post('/signin', celebrate(celebrateUser), login);
 
 router.post('/signup', createUser);
 
